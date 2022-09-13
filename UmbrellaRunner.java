501831c1ed7cc71634642f0187da3cd93beb4408
sanjay s <sanjays.xworkz@gmail.com>
2022-09-13 12:08:40 +0530

    manu anna

---
+++ UmbrellaRunner.java
@@ -0,0 +1,22 @@
+class UmbrellaRunner{
+	
+	
+	public static void main(String[]water){
+		
+		System.out.println("Running main method");
+	    Umbrella.open();
+		Umbrella.close();
+		Umbrella.rotating();
+		Umbrella.fold();
+		Umbrella.pressButten();
+		Umbrella.usedassunshade();
+		Umbrella.decoration();
+		Umbrella.turnintokite();
+		Umbrella.compact();
+		Umbrella.strong();
+		Umbrella.windproof();
+		Umbrella.lightweight();
+	
+		
+	}
+}
